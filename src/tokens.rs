e04ce058110c191b488a84f9638a8ff533b08914
Darryl <118641404+cdsiats@users.noreply.github.com>
2025-02-12 17:21:21 +0800

    added tokens and regex for Identifier and Literals

---
+++ src/tokens.rs
@@ -0,0 +1,26 @@
+use logos::Logos;
+
+#[derive(Debug,Logos,Clone,PartialEq)]
+// Skip whitespace
+#[logos(skip r"[ \t\n\f]+")]
+// Skip single line comments
+#[logos(skip r"\/\/[^\n\r]*/")]
+pub enum Token {
+    // CamelCase
+    #[regex(r#"[a-z]+(?:[A-Z][a-z]*)+"#)]
+    // PascalCase
+    #[regex(r#"[A-Z][a-z]*(?:[A-Z][a-z]*)*"#)]
+    // Lowercase
+    #[regex(r#"[a-z_][a-z0-9_]*"#)]
+    // Uppercase
+    #[regex(r#"[A-Z_][A-Z0-9_]*/i"#)]
+    Identifier,
+
+    // Integers
+    #[regex(r#"-?[0-9]+"#)]
+    // Floats
+    #[regex(r#"-?\d+\.\d+"#)]
+    // String Literals
+    #[regex(r#""([^"]+)""#)]
+    Literal
+}
